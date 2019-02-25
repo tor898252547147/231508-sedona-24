6c5cd774c4c93113c039b019d0d4e3f9b6860c76
tor898252547147 <tor898252547147@github.com>
2019-02-25 21:11:01 +0300

    форма повляется, исчезает

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var link = document.querySelector (".sinteresting-form");
+var popup = document.querySelector(".form-search");
+
+
+link.addEventListener("click", function (evt) {
+
+    popup.classList.toggle("modal-close");
+   
+});
