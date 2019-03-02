5967e1e117c5efea26b75eb6532d8fb884eb9cf9
tor898252547147 <tor898252547147@github.com>
2019-03-02 23:37:44 +0300

    третий блов - выделяется. надо доделать

--- js/main.js
+++ js/main.js
@@ -7,3 +7,12 @@ link.addEventListener("click", function (evt) {
     popup.classList.toggle("modal-close");
    
 });
+
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+    /*  if (popup.classList.contains("form-search")) {
+        popup.classList.remove("form-search");
+      }*/
+    }
+  });
